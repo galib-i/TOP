59046f44422e8db1d47182256d8132cc13b3db81
Galib <60702760+galib-i@users.noreply.github.com>
2024-10-29 10:41:26 +0000

    Complete Arrays and Loops exercises

--- javascript-exercises-main/05_sumAll/sumAll.js
+++ javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start + end)) return "ERROR";
+    if ((start < 0) || (end < 0)) return "ERROR";
 
+    let sum = 0;
+    if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    for (let i = start; i < end + 1; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
