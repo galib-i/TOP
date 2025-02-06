59e9422936ff7e8b072f35b8521d835076b3eac9
Galib <60702760+galib-i@users.noreply.github.com>
2025-02-06 14:54:40 +0000

    Clean up non-projects

--- javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++
@@ -1,6 +0,0 @@
-const removeFromArray = function(array, ...values) {
-    return array.filter(value => !values.includes(value))
-};
-
-// Do not edit below this line
-module.exports = removeFromArray;
